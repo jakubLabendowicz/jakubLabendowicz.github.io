1c33ea894f8226c76d09c335375a8fc3638f2d89
jakublabendowicz <jalabendowicz@gmail.com>
2020-04-19 14:01:16 +0200

    all

---
+++ all/panel/panel.js
@@ -0,0 +1,26 @@
+function panelOpener(panelName) {
+  var panelVisibility = document.getElementById(panelName).style.visibility;
+  if(panelVisibility == "hidden") {
+    document.getElementById('appsPanel').style.visibility = "hidden";
+    document.getElementById('allSearchPanel').style.visibility = "hidden";
+    document.getElementById('allCollectionsPanel').style.visibility = "hidden";
+
+    document.getElementById(panelName).style.visibility = "visible";
+    document.getElementById('body').style.margin = "0px 0px 0px 400px";
+    document.getElementById('bar').style.left = "400px";
+    document.getElementById('container').style.left = "60%";
+  }
+  else {
+    document.getElementById(panelName).style.visibility = "hidden";
+    document.getElementById('body').style.margin = "0px 0px 0px 0px";
+    document.getElementById('bar').style.left = "0px";
+
+    document.getElementById('appsPanel').style.visibility = "hidden";
+    document.getElementById('allSearchPanel').style.visibility = "hidden";
+    document.getElementById('allCollectionsPanel').style.visibility = "hidden";
+
+    document.getElementById('container').style.left = "50%";
+  }
+}
+
+// TODO: stworzyć id="panel", którym zastąpić pojedyńcze id paneli
