fe12988926d288284460f5c67ba444a3c0034c6c
jakublabendowicz <jalabendowicz@gmail.com>
2020-04-22 00:28:12 +0200

    contact panel

--- contactPanel.js
+++ contactPanel.js
@@ -6,16 +6,16 @@ function contactPanel() {
 
   if(contactPanelBg.style.visibility == "hidden") {
     contactPanelBg.style.visibility = "visible";
-    contactPanelBg.style.animation = "openContactPanelBgAnimation 3s 1";
+    contactPanelBg.style.animation = "openContactPanelBgAnimation 4s 1";
     setTimeout(function() {
       contactPanel.style.visibility = "visible";
-    }, 3000);
+    }, 4000);
   }
   else {
     contactPanel.style.visibility = "hidden";
-    contactPanelBg.style.animation = "closeContactPanelBgAnimation 3s 1";
+    contactPanelBg.style.animation = "closeContactPanelBgAnimation 4s 1";
     setTimeout(function() {
       contactPanelBg.style.visibility = "hidden"
-    }, 3000);
+    }, 4000);
   }
 }
