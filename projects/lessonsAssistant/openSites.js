906e0bd9c552f811fd9924ae2e0ce286e1751b72
jakublabendowicz <jalabendowicz@gmail.com>
2020-05-04 14:52:03 +0200

    lessonsAssistant

--- projects/lessonsAssistant/openSites.js
+++ projects/lessonsAssistant/openSites.js
@@ -36,9 +36,10 @@ function openSites(group) {
     openSite(1, 12, 14, 'https://moodle1.cs.pollub.pl/course/view.php?id=794');
 
     openSite(1, 14, 16, 'https://moodle1.cs.pollub.pl/course/view.php?id=750');
-    openSite(1, 14, 16, 'https://kgromaszek.my.webex.com/kgromaszek.my/e.php?MTID=m50101161ccd38e380c2cf196ef80c336');
+    openSite(1, 14, 16, 'https://kgromaszek.my.webex.com/kgromaszek.my/e.php?MTID=m50101161ccd38e380c2cf196ef80c336'); //???
 
     openSite(1, 16, 18, 'https://moodle1.cs.pollub.pl/course/view.php?id=808');
+
     openSite(1, 18, 20, 'https://moodle1.cs.pollub.pl/course/view.php?id=551');
 
     //Wtorek
