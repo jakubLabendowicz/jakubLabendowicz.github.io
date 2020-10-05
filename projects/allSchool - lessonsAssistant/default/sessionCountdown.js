6bfb72beaa9c47fff406a5afbee24d724a1fc43d
jakublabendowicz <jalabendowicz@gmail.com>
2020-10-05 17:55:26 +0200

    school

--- projects/allSchool - lessonsAssistant/default/sessionCountdown.js
+++ projects/allSchool - lessonsAssistant/default/sessionCountdown.js
@@ -2,6 +2,6 @@ function sessionCountdown() {
   const date = new Date();
   var time = date.getTime();
   time = (time/1000/60/60/24).toFixed(0);
-  var sessionTime = 18388 + 48;
-  document.getElementById('sessionCountdown').innerHTML = (sessionTime - time).toString() + " dni do sesji";
+  var sessionTime = 18388 + 48 + 105 + 121;
+  document.getElementById('sessionCountdown').innerHTML = (sessionTime - time).toString() + " days to the session";
 }
