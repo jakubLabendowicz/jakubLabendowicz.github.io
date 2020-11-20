d08abbdded4ad1ca979a1d2f6706b437bc3c5aea
jakublabendowicz <jalabendowicz@gmail.com>
2020-11-21 00:02:56 +0100

    portfolio

--- easterEggs.js
+++ easterEggs.js
@@ -9,31 +9,15 @@ function viewEasterEggs() {
     color = sessionStorage.getItem('color');
   }
   changeColor();
-
-  if(sessionStorage.getItem('world') == null) {
-    sessionStorage.setItem('world', 'hidden');
-  }
-
-  if(sessionStorage.getItem('world') == 'visible') {
-    showWorld();
-  }
 }
 
 function changeColor() {
   const colors = ['#9e9e9e', '#f44336', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548'];
   const element = document.querySelector(".container");
-  element.style.setProperty("--firstColor", colors[color]);
+  element.style.setProperty("--backgroundColor2", colors[color]);
   sessionStorage.setItem('color', color);
   color++;
   if(color == 15) {
     color = 0;
   }
-}
-
-function showWorld() {
-  const world = document.querySelector('#world');
-  world.style.padding = "0px 0px 0px 30px";
-  world.style.visibility = "visible";
-  world.style.width = "auto";
-  sessionStorage.setItem('world', 'visible');
 }
---
+++ projects/allTime/Timer.js
@@ -0,0 +1,21 @@
+class Timer {
+  constructor(time, name="Timer", delayTimer=true) {
+    this.time = time;
+    this.name = name;
+    this.delayTimer = delayTimer;
+  }
+
+  run() {
+    getElementById('title').innerHTML = this.name;
+
+  }
+}
+
+Gear = new Array();
+Gear[0] = new Timer();
+
+class Gear {
+  constructor() {
+
+  }
+}
