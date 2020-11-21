5d99699cf468a5bb352087409ff340bfd7b15f6b
jakublabendowicz <jalabendowicz@gmail.com>
2020-11-21 18:51:59 +0100

    edit

--- projects/Animations/Lava/theme.js
+++ projects/Animations/Lava/theme.js
@@ -3,10 +3,12 @@ var mode = 0;
 function brithnes() {
   if (mode == 0) {
     document.body.style.backgroundColor = "black";
+    document.getElementById('logo').style.backgroundImage = "url('JŁ!-2.png')";
     mode = 1;
   }
   else {
     document.body.style.backgroundColor = "white";
+    document.getElementById('logo').style.backgroundImage = "url('JŁ!.png')";
     mode = 0;
   }
 }
