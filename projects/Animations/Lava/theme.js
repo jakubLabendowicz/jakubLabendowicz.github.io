fe1c7bb456fc59fdabbf7e424980c4d9899188b9
jakublabendowicz <jalabendowicz@gmail.com>
2020-11-22 22:34:37 +0100

    edit

--- projects/Animations/Lava/theme.js
+++ projects/Animations/Lava/theme.js
@@ -30,8 +30,18 @@ function bubbles() {
   var bubbleTopColor = document.getElementById('bubbleTopColor').value;
   var bubbleBootomColor = document.getElementById('bubbleBootomColor').value;
 
+  var bubbleBootomColor1 = bubbleBootomColor;
+  var bubbleBootomColor2 = bubbleBootomColor;
+  var bubbleBootomColor3 = bubbleBootomColor;
+
+  if (bubbleBootomColor == "mix") {
+    bubbleBootomColor1 = "#D14F4F";
+    bubbleBootomColor2 = "#CDD14F";
+    bubbleBootomColor3 = "#4FD153";
+  }
+
   document.documentElement.style.setProperty('--bubbleTopColor', bubbleTopColor);
-  document.documentElement.style.setProperty('--bubbleBootomColor1', bubbleBootomColor);
-  document.documentElement.style.setProperty('--bubbleBootomColor2', bubbleBootomColor);
-  document.documentElement.style.setProperty('--bubbleBootomColor3', bubbleBootomColor);
+  document.documentElement.style.setProperty('--bubbleBootomColor1', bubbleBootomColor1);
+  document.documentElement.style.setProperty('--bubbleBootomColor2', bubbleBootomColor2);
+  document.documentElement.style.setProperty('--bubbleBootomColor3', bubbleBootomColor3);
 }
