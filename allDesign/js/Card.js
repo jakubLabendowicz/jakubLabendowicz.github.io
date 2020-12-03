0930c1add28820eafeb51bf8b8020e5cb0a219c4
jakubLabedowicz <jalabedowicz@gmail.com>
2020-12-03 16:21:40 +0100

    edit

--- allDesign/js/Card.js
+++ allDesign/js/Card.js
@@ -6,20 +6,20 @@ class Card {
   }
 
   open() {
-    if (!Card.previousCardId='') document.getElementById(Card.previousCardId).style.visibility = 'hidden';
+    if (!Card.previousCardId=='') document.getElementById(Card.previousCardId).style.visibility = 'hidden';
     document.getElementById(this.cardId).style.visibility = 'visible';
     Card.previousCardId = this.cardId;
   }
 
   static open(cardId) {
-    if (!Card.previousCardId='') document.getElementById(Card.previousCardId).style.visibility = 'hidden';
+    if (!Card.previousCardId=='') document.getElementById(Card.previousCardId).style.visibility = 'hidden';
     document.getElementById(cardId).style.visibility = 'visible';
     Card.previousCardId = cardId;
   }
 }
 
-Card mainCard = new Card('mainCard');
-Card settingsCard = new Card('settingsCard');
-Card appsCard = new Card('appsCard');
+mainCard = new Card('mainCard');
+settingsCard = new Card('settingsCard');
+appsCard = new Card('appsCard');
 
 mainCard.open();
