7b566610c74e596f873599a9cea82138970b4995
jakubLabedowicz <jalabedowicz@gmail.com>
2020-12-10 13:45:39 +0100

    edit

--- projects/FreestyleRandomizer/js/Arrow.js
+++ projects/FreestyleRandomizer/js/Arrow.js
@@ -1,5 +1,7 @@
 function arrow() {
   document.getElementById('container').style.animation = 'move 2s 1';
+  document.getElementById('footer').style.animation = 'move 2s 1';
+  document.getElementById('footer').style.display = 'none';
   setTimeout("window.open('https://jakublabendowicz.github.io/projects/FreestyleRandomizer/random.html', '_self')", 1000);
 }
 
