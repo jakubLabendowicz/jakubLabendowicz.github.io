cb72ad615d5f5c98f20d61c0955165c64a860b34
jakubLabedowicz <jalabedowicz@gmail.com>
2020-12-13 19:41:47 +0100

    edit

--- projects/Bubbles/Bubbles.js
+++ projects/Bubbles/Bubbles.js
@@ -1,55 +1,63 @@
-class BubblesTheme {
+class BubblesColor {
   static quantity = 0;
 
-  constructor(name="defoult", bubbleTopColor1, bubbleBootomColor1=bubbleTopColor1, bubbleTopColor2=bubbleTopColor1, bubbleBootomColor2=bubbleBootomColor1, bubbleTopColor3=bubbleTopColor1, bubbleBootomColor3=bubbleBootomColor1) {
+  constructor(name="defoult", bubbleColor1, bubbleColor2=bubbleColor1, bubbleColor3=bubbleColor1) {
     this.name = name;
-    this.number = BubblesTheme.quantity;
-    BubblesTheme.quantity++;
-    this.bubbleTopColor1 = bubbleTopColor1;
-    this.bubbleTopColor2 = bubbleTopColor2;
-    this.bubbleTopColor3 = bubbleTopColor3;
-    this.bubbleBootomColor1 = bubbleBootomColor1;
-    this.bubbleBootomColor2 = bubbleBootomColor2;
-    this.bubbleBootomColor3 = bubbleBootomColor3;
+    this.number = BubblesColor.quantity;
+    BubblesColor.quantity++;
+    this.bubbleColor1 = bubbleColor1;
+    this.bubbleColor2 = bubbleColor2;
+    this.bubbleColor3 = bubbleColor3;
   }
 
-  showTopColors() {
-    document.documentElement.style.setProperty('--bubbleTopColor1', this.bubbleTopColor1);
-    document.documentElement.style.setProperty('--bubbleTopColor2', this.bubbleTopColor2);
-    document.documentElement.style.setProperty('--bubbleTopColor3', this.bubbleTopColor3);
+  showTop() {
+    document.documentElement.style.setProperty('--bubbleTopColor1', this.bubbleColor1);
+    document.documentElement.style.setProperty('--bubbleTopColor2', this.bubbleColor2);
+    document.documentElement.style.setProperty('--bubbleTopColor3', this.bubbleColor3);
   }
 
-  showBootomColors() {
-    document.documentElement.style.setProperty('--bubbleBootomColor1', this.bubbleBootomColor1);
-    document.documentElement.style.setProperty('--bubbleBootomColor2', this.bubbleBootomColor2);
-    document.documentElement.style.setProperty('--bubbleBootomColor3', this.bubbleBootomColor3);
+  showBootom() {
+    document.documentElement.style.setProperty('--bubbleBootomColor1', this.bubbleColor1);
+    document.documentElement.style.setProperty('--bubbleBootomColor2', this.bubbleColor2);
+    document.documentElement.style.setProperty('--bubbleBootomColor3', this.bubbleColor3);
   }
 
   static bubbleThemeMode = 0;
 
   static change() {
-    if (BubblesTheme.bubbleThemeMode==0) {
-      bubblesThemes[0].showTopColors();
-      bubblesThemes[4].showBootomColors();
-      BubblesTheme.bubbleThemeMode = 1;
+    if (BubblesColor.bubbleThemeMode==0) {
+      bubblesThemes[0].showTop();
+      bubblesThemes[4].showBootom();
+      BubblesColor.bubbleThemeMode = 1;
     }
     else {
-      bubblesThemes[1].showTopColors();
-      bubblesThemes[4].showBootomColors();
-      BubblesTheme.bubbleThemeMode = 0;
+      bubblesThemes[1].showTop();
+      bubblesThemes[4].showBootom();
+      BubblesColor.bubbleThemeMode = 0;
     }
   }
 }
 
-bubblesThemes = new Array();
-bubblesThemes[0] = new BubblesTheme("white", "white");
-bubblesThemes[1] = new BubblesTheme("black", "black");
-bubblesThemes[2] = new BubblesTheme("red", "#D14F4F");
-bubblesThemes[3] = new BubblesTheme("green", "#4FD153");
-bubblesThemes[4] = new BubblesTheme("blue", "#4f7fd1");
-bubblesThemes[5] = new BubblesTheme("yellow", "#CDD14F");
+class BubblesTheme {
+  constructor(lightTop, lightBottom, darkTop, darkBottom) {
+    this.lightTop = lightTop;
+    this.lightBottom = lightBottom;
+    this.darkTop = darkTop;
+    this.darkBottom = darkBottom;
+  }
 
-BubblesTheme.change();
+  
+}
+
+bubblesThemes = new Array();
+bubblesThemes[0] = new BubblesColor("white", "white");
+bubblesThemes[1] = new BubblesColor("black", "black");
+bubblesThemes[2] = new BubblesColor("red", "#D14F4F");
+bubblesThemes[3] = new BubblesColor("green", "#4FD153");
+bubblesThemes[4] = new BubblesColor("blue", "#4f7fd1");
+bubblesThemes[5] = new BubblesColor("yellow", "#CDD14F");
+
+BubblesColor.change();
 
 
 function bubbles() {
@@ -67,7 +75,8 @@ function bubbles() {
     bubbleBootomColor3 = "#4FD153";
   }
 
-  var bubblesTheme = new BubblesTheme("", bubbleTopColor, bubbleBootomColor1, bubbleTopColor, bubbleBootomColor2, bubbleTopColor, bubbleBootomColor3);
-  bubblesTheme.showTopColors();
-  bubblesTheme.showBootomColors();
+  var bubblesTopTheme = new BubblesColor("", bubbleTopColor, bubbleTopColor, bubbleTopColor);
+  var bubblesBottomTheme = new BubblesColor("", bubbleBootomColor1, bubbleBootomColor2, bubbleBootomColor3);
+  bubblesTopTheme.showTop();
+  bubblesBottomTheme.showBootom();
 }
