33d3f6bb4075330b2f1c41a40f0c3936e5d66543
jakubLabedowicz <jalabedowicz@gmail.com>
2021-03-13 11:10:23 +0100

    edit

--- projects/allSchool/js/sessionCountdown.js
+++
@@ -1,7 +0,0 @@
-function sessionCountdown() {
-  const date = new Date();
-  var time = date.getTime();
-  time = (time/1000/60/60/24).toFixed(0);
-  var sessionTime = 18388 + 48 + 105 + 121;
-  document.getElementById('sessionCountdown').innerHTML = (sessionTime - time).toString() + " days to the exams";
-}
