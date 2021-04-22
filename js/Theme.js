6cd62b54ba04fcd324af02614a91887f2a7f84dd
jakubLabedowicz <jalabedowicz@gmail.com>
2021-04-22 13:30:04 +0200

    edit

--- js/Theme.js
+++ js/Theme.js
@@ -6,6 +6,8 @@ light.add("--cardTextHoverColor", "#000000");
 light.add("--projectShadowColor", "#00000029");
 light.add("--CVBackgroundColor", "#fafafa");
 light.add("--GHImage", "url('../graphics/GitHub-Mark-120px-plus.png')");
+light.add("--QRImage", "url('../graphics/QRDark.png')");
+
 
 // var dimmed = new Theme("Dimmed");
 // dimmed.add("--mainBackgroundColor", "#ffffff");
@@ -15,6 +17,7 @@ light.add("--GHImage", "url('../graphics/GitHub-Mark-120px-plus.png')");
 // dimmed.add("--projectShadowColor", "#00000029");
 // dimmed.add("--CVBackgroundColor", "#fafafa");
 //dimmed.add("--GHImage", "url('../graphics/GitHub-Mark-120px-plus.png')");
+//dimmed.add("--QRImage", "url('../graphics/QRLight.png')");
 
 var dark = new Theme("Dark");
 dark.add("--mainBackgroundColor", "#212121");
@@ -24,6 +27,7 @@ dark.add("--cardTextHoverColor", "#ffffff");
 dark.add("--projectShadowColor", "#000000");
 dark.add("--CVBackgroundColor", "#1a1a1a");
 dark.add("--GHImage", "url('../graphics/GitHub-Mark-Light-64px.png')");
+dark.add("--QRImage", "url('../graphics/QRLight.png')");
 
 var themeController = new ThemeController("themeController");
 themeController.addTheme(light);
