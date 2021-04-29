3d873810f618226d472d6c12f0b5bafe370fad12
jakubLabedowicz <jalabedowicz@gmail.com>
2021-04-29 18:19:47 +0200

    edit

--- js/repos.js
+++ js/repos.js
@@ -5,7 +5,7 @@ function viewRepos() {
       for (var i = 0; i < res.length; i++) {
         console.log(res[i].name + "\n" + res[i].html_url + "\n" + "https://labendowicz.pl/" + res[i].name);
         const $repositoriesContainer = $("#repositoriesContainer");
-        const $project = $("<div class='project flex-column-container'><div class='project-logo'></div><div class='project-container'><div class='project-header'>"
+        const $project = $("<div class='project flex-column-container'><div class='project-logo' style='height: 20px'></div><div class='project-container'><div class='project-header'>"
           + res[i].name
           + "</div><div class='flex-column-container' style='gap: 0px'><a href='"
           + "https://labendowicz.pl/"
